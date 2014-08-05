a5453c22cfde53f7e3bf9f49012e974df48afd45
Agoston Horvath <agoston@ripe.net>
2014-08-05 17:36:34 +0200

    3 done

--- src/main/java/org/onehippo/assessment/Application.java
+++ src/main/java/org/onehippo/assessment/Application.java
@@ -8,23 +8,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jcr.*;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryManager;
+import javax.jcr.query.QueryResult;
 import java.util.Iterator;
-import java.util.Spliterators;
-import java.util.stream.StreamSupport;
 
 public final class Application {
 
     private static Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) throws RepositoryException {
-        String repoUrl = "rmi://localhost:1099/hipporepository";
-        String username = "admin";
-        char[] password = "admin".toCharArray();
-
-        HippoRepository repository = HippoRepositoryFactory.getHippoRepository(repoUrl);
-        Session session = repository.login(username, password);
-        Node rootNode = session.getRootNode();
-
+    private static void showcaseTraverse(final Node rootNode) throws RepositoryException {
         for (Iterator<Node> it = TreeTraverser.nodeIterator(rootNode.getNode("content")); it.hasNext(); ) {
             Node node = it.next();
             if (node.getIdentifier().startsWith("cafeface")) continue;
@@ -38,5 +31,34 @@ public final class Application {
                         (property.isMultiple() ? StringUtils.join(property.getValues(), ", ") : property.getValue()));
             }
         }
+    }
+
+    private static void showcaseQuery(final Session session) throws RepositoryException {
+        QueryManager queryManager = session.getWorkspace().getQueryManager();
+        Query query = queryManager.createQuery("SELECT a.* FROM [nt:base] AS a WHERE CONTAINS(a.*, 'Files')", Query.JCR_SQL2);
+
+        QueryResult queryResult = query.execute();
+
+        for (NodeIterator it = queryResult.getNodes(); it.hasNext(); ) {
+            Node node = it.nextNode();
+
+            System.out.println(node.getPath());
+        }
+    }
+
+    public static void main(String[] args) throws RepositoryException {
+        String repoUrl = "rmi://localhost:1099/hipporepository";
+        String username = "admin";
+        char[] password = "admin".toCharArray();
+
+        HippoRepository repository = HippoRepositoryFactory.getHippoRepository(repoUrl);
+        Session session = repository.login(username, password);
+        Node rootNode = session.getRootNode();
+
+//        showcaseTraverse(rootNode);
+//        showcaseQuery(session);
+
+        session.logout();
+
     }
 }
