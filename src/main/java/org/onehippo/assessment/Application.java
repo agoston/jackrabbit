fa4e9cea26ed345d8050afdee0a7d3d6890306f0
Agoston Horvath <agoston@ripe.net>
2014-08-05 15:59:46 +0200

    test version with streams; trying without (too much pain ftm)

--- src/main/java/org/onehippo/assessment/Application.java
+++ src/main/java/org/onehippo/assessment/Application.java
@@ -1,11 +1,5 @@
 package org.onehippo.assessment;
 
-import javax.jcr.Node;
-import javax.jcr.Property;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
-
-import org.apache.commons.collections.IteratorUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.jackrabbit.commons.flat.TreeTraverser;
 import org.hippoecm.repository.HippoRepository;
@@ -13,42 +7,14 @@ import org.hippoecm.repository.HippoRepositoryFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.jcr.*;
 import java.util.Iterator;
 import java.util.Spliterators;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-/**
- * @version "$Id$"
- */
 public final class Application {
 
     private static Logger log = LoggerFactory.getLogger(Application.class);
-
-    static class PrintNodePropertiesConsumer implements Consumer<Node> {
-        @Override
-        public void accept(Node node) {
-            try {
-                System.out.print(node.getPath());
-                System.out.print(": ");
-                System.out.println(StringUtils.join(node.getProperties(), ", "));
-            } catch (RepositoryException e) {
-                throw new RuntimeException("Should not happen", e);
-            }
-        }
-    }
-
-    // TODO: should not have checked exceptions or should have utility methods to do this silently
-    private static String getUuid(final Node node) {
-        try {
-            return node.getIdentifier();
-        } catch (RepositoryException e) {
-            throw new RuntimeException("Should not happen", e);
-        }
-    }
-
 
     public static void main(String[] args) throws RepositoryException {
         String repoUrl = "rmi://localhost:1099/hipporepository";
@@ -59,9 +25,26 @@ public final class Application {
         Session session = repository.login(username, password);
         Node rootNode = session.getRootNode();
 
-        StreamSupport.stream(Spliterators.spliteratorUnknownSize(TreeTraverser.nodeIterator(rootNode), 0), false)
-                .filter((node) -> getUuid(node).contains("cafeface"))
-                .forEach(new PrintNodePropertiesConsumer());
+        // TODO: have a runtimeexception version of Node; we can't even put the nodeId in an exception message like this
+        StreamSupport.stream(Spliterators.spliteratorUnknownSize(TreeTraverser.nodeIterator(rootNode), 0), true)
+                .filter(node -> {
+                    try {
+                        return node.getIdentifier().contains("cafe");   // TODO: confirm that uuid is forced lowercase
+                    } catch (RepositoryException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .forEach(node -> {
+                    try {
+                        System.out.println(node.getPath() + ": " + node.getIdentifier());
+                        for (PropertyIterator it = node.getProperties(); it.hasNext(); ) {
+                            Property property = it.nextProperty();
+                            System.out.println("\t\t" + property.getName() + " = " + StringUtils.join(property.getValues(), ", "));
+                        }
+                    } catch (RepositoryException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
 
 
     }
