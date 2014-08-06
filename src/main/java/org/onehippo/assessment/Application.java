d52c0c7f280d39122380a2fb47c37deeec4e783e
Agoston Horvath <agoston@ripe.net>
2014-08-06 15:16:49 +0200

    4th done (but fails on unimplemented method; removing with TODO)

--- src/main/java/org/onehippo/assessment/Application.java
+++ src/main/java/org/onehippo/assessment/Application.java
@@ -8,31 +8,46 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jcr.*;
+import javax.jcr.observation.Event;
+import javax.jcr.observation.EventIterator;
+import javax.jcr.observation.EventListener;
+import javax.jcr.observation.ObservationManager;
 import javax.jcr.query.Query;
 import javax.jcr.query.QueryManager;
 import javax.jcr.query.QueryResult;
+import java.text.DateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public final class Application {
 
-    private static Logger log = LoggerFactory.getLogger(Application.class);
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    private static CountDownLatch observationFinished = new CountDownLatch(5);
 
     private static void showcaseTraverse(final Node rootNode) throws RepositoryException {
         for (Iterator<Node> it = TreeTraverser.nodeIterator(rootNode.getNode("content")); it.hasNext(); ) {
             Node node = it.next();
             if (node.getIdentifier().startsWith("cafeface")) continue;
 
-            System.out.println(node.getPath() + ": " + node.getIdentifier());
+            System.err.println(node.getPath() + ": " + node.getIdentifier());
 
             for (PropertyIterator pi = node.getProperties(); pi.hasNext(); ) {
                 Property property = pi.nextProperty();
 
-                System.out.println("\t\t" + property.getName() + "[" + PropertyType.nameFromValue(property.getType()) + "] = " +
+                System.err.println("\t\t" + property.getName() + "[" + PropertyType.nameFromValue(property.getType()) + "] = " +
                         (property.isMultiple() ? StringUtils.join(property.getValues(), ", ") : property.getValue()));
             }
         }
     }
 
+    // TODO: check if repo supports query
     private static void showcaseQuery(final Session session) throws RepositoryException {
         QueryManager queryManager = session.getWorkspace().getQueryManager();
         Query query = queryManager.createQuery("SELECT a.* FROM [nt:base] AS a WHERE CONTAINS(a.*, 'Files')", Query.JCR_SQL2);
@@ -42,11 +57,11 @@ public final class Application {
         for (NodeIterator it = queryResult.getNodes(); it.hasNext(); ) {
             Node node = it.nextNode();
 
-            System.out.println(node.getPath());
+            System.err.println(node.getPath());
         }
     }
 
-    public static void main(String[] args) throws RepositoryException {
+    public static void main(String[] args) throws Exception {
         String repoUrl = "rmi://localhost:1099/hipporepository";
         String username = "admin";
         char[] password = "admin".toCharArray();
@@ -58,7 +73,56 @@ public final class Application {
 //        showcaseTraverse(rootNode);
 //        showcaseQuery(session);
 
+        // TODO: check if repo supports observations
+        ObservationManager observationManager = session.getWorkspace().getObservationManager();
+        NodeEventListener listener = new NodeEventListener(observationFinished);
+        observationManager.addEventListener(listener, Event.NODE_ADDED | Event.NODE_MOVED | Event.NODE_REMOVED, "/content", true, null, null, false);
+
+        System.err.println("Now waiting...");
+        observationFinished.await(1, TimeUnit.DAYS);
+
+        observationManager.removeEventListener(listener);
+
         session.logout();
 
+    }
+
+    private static final String formatEventType(int eventType) {
+        switch (eventType) {
+            case Event.NODE_MOVED:
+                return "MOVED";
+            case Event.NODE_REMOVED:
+                return "REMOVED";
+            case Event.NODE_ADDED:
+                return "ADDED";
+            default:
+                throw new UnsupportedOperationException();
+        }
+    }
+
+
+    private static class NodeEventListener implements EventListener {
+
+        private final CountDownLatch observationFinished;
+
+        public NodeEventListener(CountDownLatch observationFinished) {
+            this.observationFinished = observationFinished;
+        }
+
+        @Override
+        public void onEvent(EventIterator events) {
+            while (events.hasNext()) {
+                try {
+                    Event event = events.nextEvent();
+                    // TODO: event.getDate() should be implemented
+                    System.err.println(LocalDateTime.ofInstant(Instant.ofEpochMilli(event.getDate()), ZoneOffset.UTC) + " " +
+                            event.getPath() + " " +
+                            formatEventType(event.getType()));
+                    observationFinished.countDown();
+                } catch (RepositoryException e) {
+                    log.error("listener", e);
+                }
+            }
+        }
     }
 }
